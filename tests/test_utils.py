83e5c16c41be6d30f728cf2f9f9a326e1e5575f7
Bea Malsky <beamalsky@datamade.us>
2020-03-08 18:06:48 -0500

    Use utcnow()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 from utils import average_interp, average_interp_timestamp
-import pandas as pd 
+import pandas as pd
 
 def test_average_interp():
     x = [0,2,4]
@@ -15,9 +15,9 @@ def test_average_interp():
     assert average_interp(y, x, -1, 5) == 12/6
 
 def test_average_interp_timestamp():
-    now = pd.Timestamp.now()
-    t = [now, 
-         now + pd.Timedelta(2, 'h'), 
+    now = pd.Timestamp.utcnow()
+    t = [now,
+         now + pd.Timedelta(2, 'h'),
          now + pd.Timedelta(4, 'h')]
     t0 = now - pd.Timedelta(1, 'h')
     t1 = now + pd.Timedelta(5, 'h')
